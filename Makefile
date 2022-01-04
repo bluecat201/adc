ac08c42809948727dc00fa2fcdfdd89f0a560036
David <56869788+bluecat201@users.noreply.github.com>
2022-01-04 09:56:16 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,39 @@
+
+# Switch Makefile
+
+MKDIR = mkdir
+ifeq ($(OS),Windows_NT)
+    uname_S := Windows
+
+	LN = cp
+else
+    uname_S := $(shell uname -s)
+
+	LN = ln -sf
+endif
+CP = cp
+
+
+menu::
+	@echo "# Vyber si toolchain!"
+	@echo
+	@echo "# Pokud chceš debug a nevadí ti větší binárka, zavolej:"
+	@echo \"make sdcc\"   ... nebo
+	@echo \"make default\"
+	@echo
+	@echo "# Pokud chceš malé binárky a nepotřebuješ debug zavolej:"
+	@echo \"make sdccrm\"
+	@echo
+	@echo "# Pokud chceš malé binárky i debug a máš 'sdcc-gas' zavolej:"
+	@echo \"make sdcc-gas\"
+
+default: sdcc
+sdcc::
+	$(LN) .make/Makefile-sdcc Makefile || cp .make/Makefile-sdcc Makefile
+sdccrm::
+	$(LN) .make/Makefile-sdccrm Makefile || cp .make/Makefile-sdccrm Makefile
+sdcc-gas::
+	$(LN) .make/Makefile-sdcc-gas Makefile || cp .make/Makefile-sdcc-gas Makefile
+
+spl::
+	bash .make/spl.sh
---
+++ src/main.c
@@ -0,0 +1,51 @@
+#include "stm8s.h"
+#include "milis.h"
+
+/*#include "delay.h"*/
+/*#include <stdio.h>*/
+/*#include "../lib/uart.c"*/
+
+#define _ISOC99_SOURCE
+#define _GNU_SOURCE
+
+#define LED_PORT GPIOC
+#define LED_PIN  GPIO_PIN_5
+#define LED_HIGH   GPIO_WriteHigh(LED_PORT, LED_PIN)
+#define LED_LOW  GPIO_WriteLow(LED_PORT, LED_PIN)
+#define LED_TOGG GPIO_WriteReverse(LED_PORT, LED_PIN)
+
+#define BTN_PORT GPIOE
+#define BTN_PIN  GPIO_PIN_4
+#define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN)==RESET) 
+
+
+void setup(void)
+{
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
+    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    init_milis();
+}
+
+
+int main(void)
+{
+    uint32_t time = 0;
+
+    setup();
+    /*init_uart();*/
+
+    while (1) {
+
+        if (milis() - time > 333 && BTN_PUSH) {
+            LED_TOGG; 
+            time = milis();
+        }
+
+        /*LED_FLIP; */
+        /*_delay_ms(333);*/
+        /*printf("Funguje to!!!\n");*/
+    }
+}
+
+/*-------------------------------  Assert -----------------------------------*/
+#include "__assert__.h"
